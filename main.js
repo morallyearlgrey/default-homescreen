fcc230ef8a4833842ad897d510fd87c42448243f
º • 🌱 kai 🌱 • º <kaisprunger@gmail.com>
2024-10-04 08:31:37 -0400

    Finalized default homescreen with a toggle lightbulb.

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
+if (typeof window != 'undefined') {
+
+window.onload = function() {
     const lightbulb = document.querySelector("#lightbulb");
 
     function changeBackground() {
-        const currentBackgroundImage = getComputedStyle(document.body).backgroundImage;
-        if (currentBackgroundImage==("./images/background.png")) {
-            document.body.style.backgroundImage = ""; 
-        } else {
+        let currentBackgroundImage = document.body.style.backgroundImage;
+        
+        if (currentBackgroundImage.includes("./images/background.png")) {
             document.body.style.backgroundImage = "url('./images/blackspace.png')";
+            const ntitle = document.getElementById("ntitle");
+            ntitle.style.color="white";
+
+            const nsubheading = document.getElementById("nsubheading");
+            nsubheading.style.color="white";
+
+        } else {
+            document.body.style.backgroundImage = "url('./images/background.png')"; 
+            const ntitle = document.getElementById("ntitle");
+            ntitle.style.color="black";
+
+            const nsubheading = document.getElementById("nsubheading");
+            nsubheading.style.color="black";
+
         }
     }
 
@@ -14,4 +29,25 @@ document.addEventListener("DOMContentLoaded", function () {
         changeBackground();
         lightbulb.classList.toggle('active'); 
     });
-});
+
+};
+
+}
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const lightbulb = document.querySelector("#lightbulb");
+
+//     function changeBackground() {
+//         const currentBackgroundImage = getComputedStyle(document.body).backgroundImage;
+//         if (currentBackgroundImage==("./images/background.png")) {
+//             document.body.style.backgroundImage = ""; 
+//         } else {
+//             document.body.style.backgroundImage = "url('./images/blackspace.png')";
+//         }
+//     }
+
+//     lightbulb.addEventListener("click", (e) => {
+//         changeBackground();
+//         lightbulb.classList.toggle('active'); 
+//     });
+// });
